7f113b25121ceb3eb51489941c3b825f29771dad
Roxana <ms.roxanarahimi@gmail.com>
2023-12-28 21:55:34 +0330

    add components

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="w-100 px-4 text-light my-font" style="background: url('/img/loginBack.svg') top center no-repeat; background-size: cover">
+  <div class="w-100 px-4 text-light " style="background: url('/img/loginBack.svg') top center no-repeat; background-size: cover">
 
-    <h2 class="my-color mt-5 text-center">سبد خرید</h2>
+    <h2 class="my-color mt-5 text-center my-font">سبد خرید</h2>
 
-   <div class="px-4 mt-5" style="">
+   <div class="px-4 mt-5 my-font" style="">
      <div class="mb-3">
        <div class="d-flex justify-content-between">
-         <h4>خمیر ساده 3 کیلو 5 بسته</h4>
+         <h4>خمیر ساده 3 کیلویی 5 بسته</h4>
          <div>
            <img src="/img/minus.png" alt="">
          </div>
@@ -17,7 +17,7 @@
      </div>
      <div class="mb-3">
        <div class="d-flex justify-content-between">
-         <h4>خمیر ساده 3 کیلو 5 بسته</h4>
+         <h4>خمیر ساده 3 کیلویی 5 بسته</h4>
          <div>
            <img src="/img/minus.png" alt="">
          </div>
@@ -28,7 +28,7 @@
      </div>
      <div class="mb-3">
        <div class="d-flex justify-content-between">
-         <h4>خمیر ساده 3 کیلو 5 بسته</h4>
+         <h4>خمیر ساده 3 کیلویی 5 بسته</h4>
          <div>
            <img src="/img/minus.png" alt="">
          </div>
@@ -40,12 +40,31 @@
    </div>
 
 
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center my-font">
       <router-link to="/products" class="btn-orange2 text-black pt-3">
         <i class="bi bi-plus"></i>
         افزودن محصول</router-link>
     </div>
 
+    <div class="my-border mt-5 p-3 text-center">
+      <h3> مبلغ نهایی: 500.000.000</h3>
+      <h3> تخفیف ریالی: 5.000.000</h3>
+    </div>
+
+
+    <div class="d-flex justify-content-between mt-5">
+
+      <p class="mb-0 my-color mt-2">نحوه تسویه</p>
+
+      <div class="d-flex">
+        <button class="btn-black2 me-3" style="width: 80px !important">نقدی</button>
+        <button class="btn-orange2" style="width: 80px !important">چک</button>
+      </div>
+    </div>
+
+    <div class="d-flex mt-5 justify-content-center">
+      <button class="btn-orange2">ثبت سفارش</button>
+    </div>
   </div>
 </template>
 
